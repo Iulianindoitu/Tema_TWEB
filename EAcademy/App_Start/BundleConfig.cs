308b7b6e0bfafda63af12b1e4d5a0e9134c3e49f
iulian indoitu <iulian.indoitu@mail.ru>
2025-03-05 20:28:02 +0200

    Add project files.

---
+++ EAcademy/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+using System.Web;
+
+namespace EAcademy.App_Start
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+
+		}	
+	}
+}
